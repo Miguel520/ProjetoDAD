c913b932f011b88c0a00ebfa95597213b4ee829c
Duarte Alves <Duarte's MBP>
2020-10-17 18:28:46 +0100

    Server: Parse Args and extend ServerConfigurationService

---
+++ Server/Configuration/ConfigurationService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Common.Protos.ServerConfiguration;
+using Grpc.Core;
+
+namespace Server.Configuration {
+    public class ConfigurationService : ServerConfigurationService.ServerConfigurationServiceBase {
+        public ConfigurationService() {
+        }
+
+        public override Task<JoinPartitionResponse> JoinPartition(JoinPartitionRequest request, ServerCallContext context) {
+            return base.JoinPartition(request, context);
+        }
+
+        public override Task<StatusResponse> Status(StatusRequest request, ServerCallContext context) {
+            return base.Status(request, context);
+        }
+
+        public override Task<CrashResponse> Crash(CrashRequest request, ServerCallContext context) {
+            return base.Crash(request, context);
+        }
+
+        public override Task<FreezeResponse> Freeze(FreezeRequest request, ServerCallContext context) {
+            return base.Freeze(request, context);
+        }
+
+        public override Task<UnfreezeResponse> Unfreeze(UnfreezeRequest request, ServerCallContext context) {
+            return base.Unfreeze(request, context);
+        }
+    }
+}
---
+++ Server/Configuration/ServerConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Common.Utils;
+
+namespace Server.Configuration {
+    public class ServerConfiguration {
+        public ServerConfiguration(int serverId, string host, int port) {
+            ServerId = serverId;
+            Host = host;
+            Port = port;
+        }
+
+        public int ServerId { get; }
+        public string Host { get; }
+        public int Port { get; }
+        public string Url { get { return HttpURLs.FromHostAndPort(Host, Port); } }
+    }
+}
