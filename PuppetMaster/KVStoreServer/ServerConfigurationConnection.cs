4383d745339bdc011656496bb6c9fc515584fef3
Duarte Alves <Duarte's MBP>
2020-10-23 18:53:36 +0100

    Closes #10, #15: PuppetMaster sends Join Partition, join partition request now has master id

---
+++ PuppetMaster/KVStoreServer/ServerConfigurationConnection.cs
@@ -0,0 +1,51 @@
+﻿using Common.Protos.ServerConfiguration;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+
+using static Common.Protos.ServerConfiguration.ServerConfigurationService;
+
+namespace PuppetMaster.KVStoreServer {
+    public class ServerConfigurationConnection {
+
+        private readonly string target;
+        private readonly Channel channel;
+        private readonly ServerConfigurationServiceClient client;
+
+        public ServerConfigurationConnection(string url) {
+            target = url;
+            channel = new Channel(target, ChannelCredentials.Insecure);
+            client = new ServerConfigurationServiceClient(channel);
+            Console.WriteLine("Established connection to {0}", target);
+        }
+
+        ~ServerConfigurationConnection() {
+            Console.WriteLine("Shutting down connection to {0}", target);
+            channel.ShutdownAsync().Wait();
+        }
+
+        public bool JoinPartition(
+            string partitionName,
+            IEnumerable<Tuple<int, string>> servers,
+            int master) {
+
+            JoinPartitionRequest request =
+                ServerConfigurationMessageFactory.BuildJoinPartitionRequest(
+                    partitionName,
+                    servers,
+                    master);
+
+            try {
+                client.JoinPartition(request);
+                return true;
+            }
+            catch (RpcException e) {
+                Console.WriteLine(
+                    "Error: {0} when joining partition at server {1}",
+                    e.StatusCode,
+                    target);
+                return false;
+            }
+        }
+    }
+}
---
+++ PuppetMaster/KVStoreServer/ServerConfigurationMessageFactory.cs
@@ -0,0 +1,35 @@
+﻿using Common.Protos.ServerConfiguration;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PuppetMaster.KVStoreServer {
+
+    public class ServerConfigurationMessageFactory {
+        private ServerConfigurationMessageFactory() {
+        }
+
+        public static JoinPartitionRequest BuildJoinPartitionRequest(
+            string partitionName,
+            IEnumerable<Tuple<int,string>> servers,
+            int masterId) {
+
+            return new JoinPartitionRequest {
+                PartitionName = partitionName,
+                Servers = { BuildServers(servers) },
+                MasterId = masterId
+            };
+        }
+
+        private static IEnumerable<Server> BuildServers(
+            IEnumerable<Tuple<int, string>> servers) {
+
+            return servers.Select(tuple => {
+                return new Server {
+                    Id = tuple.Item1,
+                    Url = tuple.Item2
+                };
+            });
+        }
+    }
+}
