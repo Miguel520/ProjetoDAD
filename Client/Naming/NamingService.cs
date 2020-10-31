2c20eb8e3d70e29d8a814c03c280ffa1aa91bea7
Duarte Alves <Duarte's MBP>
2020-10-31 10:02:06 +0000

    Issue 54: Client List All Partitions at start

--- Client/Naming/NamingService.cs
+++ Client/Naming/NamingService.cs
@@ -8,11 +8,20 @@ namespace Client.Naming {
         private readonly ImmutableList<string> nameServersUrls;
         private readonly Dictionary<int, string> knownServers;
         private readonly Dictionary<string, string> knownMasters;
+        private readonly ImmutableDictionary<string, ImmutableHashSet<int>> partitions;
 
         public NamingService(ImmutableList<string> nameServersUrls) {
             this.nameServersUrls = nameServersUrls;
             knownServers = new Dictionary<int, string>();
             knownMasters = new Dictionary<string, string>();
+            foreach (string nameServerUrl in nameServersUrls) {
+                NamingServiceConnection connection =
+                    new NamingServiceConnection(nameServerUrl);
+
+                if (connection.ListPartitions(out partitions)) {
+                    break;
+                }
+            }
         }
 
         public bool Lookup(int id, out string url) {
@@ -45,6 +54,6 @@ namespace Client.Naming {
             }
 
             return (url != null);
-        }
+        }     
     }
 }
